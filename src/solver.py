59bef17d115b0c0e2ea9e170cfc614dc1261bbbc
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-05-18 12:46:08 +0300

    implement the solve function

--- src/solver.py
+++ src/solver.py
@@ -18,4 +18,45 @@ class MazeSolver:
 
 
     def solve(self,start,end):
+        self.current = start
+        self.end = end
+
+        self.stack.append(self.current)
+        self.visited.add(self.current)
+
+        while self.stack:
+            self.current = self.stack[-1]
+
+            self.path = list(self.stack)
+
+            # Draw animation
+            self.draw_state()
+
+            if self.current == end:
+                self.finished = True
+                print("Maze solved!")
+                return True
+
+            neighbors = self.maze.open_neighbors(
+                self.current[0],
+                self.current[1]
+            )
+
+            unvisited = [
+                cell for cell in neighbors
+                if cell not in self.visited
+            ]
+
+            if unvisited:
+                next_cell = random.choice(unvisited)
+                self.stack.append(next_cell)
+                self.visited.add(next_cell)
+            else:
+                self.dead_ends.add(self.current)
+                self.stack.pop()
+
+        print("No solution found")
+        return False
+    
+    def draw_state(self):
         pass
